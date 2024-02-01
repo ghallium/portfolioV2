e5c7368654d3bb6aa0bfc4b2bbde0e171c587a94
ghallium <guillaumerober@gmail.com>
2024-02-01 19:08:32 +0100

    404 wip

--- src/pages/error/error.jsx
+++ src/pages/error/error.jsx
@@ -4,17 +4,19 @@ import mobileError from "../../assets/mobile-error.png"
 
 function Error() {
     return (
-    <div className="error-pageblock">
+    <div className="error-pageblock flex bg-zinc-100 text-zinc-700 dark:bg-zinc-800 dark:text-zinc-100">
         <div className="error-image">
             <img src={iamError} alt="je suis une erreur 404"/>
         </div>
-        <div className="error-image-mobile">
+        <div className="error-image-mobile md:hidden">
             <img src={mobileError} alt="je suis une erreur 404"/>
         </div>
         <div className="error-info">
-            <h1>404</h1>
-            <p>La page que vous avez demandé n'existe pas.</p>
-            <a href="/">Revenir à la page d'accueil</a>
+            <h1 className="text-9xl py-6 mx-5 font-light">4<span className="text-ghgreen font-bold">04</span></h1>
+            <div className="mx-5">
+                <p className="mb-4">La page que vous avez demandé n'existe pas.</p>
+                <a href="/" className="font-bold hover:text-ghgreen transition-all">Revenir à la page d'accueil</a>
+            </div>
         </div>
     </div>
     )
--- src/pages/project/project.jsx
+++ src/pages/project/project.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Error from "../error/error";
